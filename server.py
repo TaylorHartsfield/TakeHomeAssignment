870b5f99cdaea4f8a91e0cd87d1f2815a5ac714e
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-10-08 17:50:29 -0600

    test front end to server connection working

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
 import model
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/members')
+def available():
+
+    return jsonify({"members" : ["message1", "message2", "message3"]})
 
 if __name__ == "__main__":
     model.connect_to_app(app, "melonAppointments")
-    app.run(debug=True)
-
+    app.run(debug=True, host="0.0.0.0")
